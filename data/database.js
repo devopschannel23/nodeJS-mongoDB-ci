a694d9166ce729995517af3db9166e94999bf99e
devopschannel23 <devops.channel23@gmail.com>
2025-12-20 16:55:59 +0530

    first commit of node js mongo project

---
+++ data/database.js
@@ -0,0 +1,25 @@
+import { MongoClient } from 'mongodb';
+
+const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
+const dbUser = process.env.MONGODB_USERNAME;
+const dbPassword = process.env.MONGODB_PASSWORD;
+const dbName = process.env.MONGODB_DB_NAME;
+
+const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
+
+console.log('Hang On! Connecting to your DB...');
+
+try {
+  await client.connect();
+  await client.db(dbName).command({ ping: 1 });
+  console.log('Connected to your DB Server');
+} catch (error) {
+  console.log('Oops! Connection failed.');
+  await client.close();
+  console.log('See you soon! Connection closed.');
+}
+
+const database = client.db(dbName);
+
+export default database;
